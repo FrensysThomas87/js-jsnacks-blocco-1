1c68ddee8ab4860bc73cb0b771a281ed2a792a69
FrensysThomas87 <delta873@gmail.com>
2020-12-18 17:54:14 +0100

    Aggiunti commenti a snack 2

--- 2/js/script.js
+++ 2/js/script.js
@@ -1,9 +1,12 @@
+//Chiedo all'utente di inserire due parole
 var primaParola = prompt('Inserisci la prima parola');
 var secondaParola = prompt('Inserisci la seconda parola');
 
+//Controllo che inserisca parole e non numeri
 if(!isNaN(primaParola) || !isNaN(secondaParola)){
   console.log('Non puoi inserire numeri');
 }else{
+  //Faccio stamapare per prima la parola più lunga e per seconda la più corta
   if(primaParola.length > secondaParola.length){
     console.log(primaParola + ' ' + secondaParola);
   }else{
