8018dcd5c46039c0fa2fa8e16878b1900e93353d
FrensysThomas87 <delta873@gmail.com>
2020-12-18 17:00:16 +0100

    corretto snack 9

--- 9/js/script.js
+++ 9/js/script.js
@@ -1,20 +1,15 @@
-var numero = prompt('Inserisci un numero di 10 cifre');
+
 
 var somma = 0;
 
 var media = 0;
 
-if(numero.length !== 10){
-  alert('Il numero deve essere di 10 cifre');
-}else{
-  for(var i = 0; i < numero.length; i++){
-    somma += parseInt(numero[i]);
+
+  for(var i = 1; i <= 10; i++){
+    somma += i;
     media = somma / 10;
+
   }
 
   console.log('La somma è ' + somma);
   console.log('La media è ' + media);
-
-
-
-}
