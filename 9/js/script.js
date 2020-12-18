13c32d4354e97eebcb6f5b842cafe834ced532e8
FrensysThomas87 <delta873@gmail.com>
2020-12-18 18:06:42 +0100

    Aggiunti commenti su snack 9

--- 9/js/script.js
+++ 9/js/script.js
@@ -4,9 +4,13 @@ var somma = 0;
 
 var media = 0;
 
-
+// Ciclo 1 numeri da 1 a 10
   for(var i = 1; i <= 10; i++){
+
+    //Sommo i numeri da 1 a 10
     somma += i;
+
+    //Faccio la media dei numeri da 1 a 10
     media = somma / 10;
 
   }
