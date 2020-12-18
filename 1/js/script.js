78b1dcdaacff415662337b32083b94f8ab5f546e
FrensysThomas87 <delta873@gmail.com>
2020-12-18 17:51:40 +0100

    Inseriti commenti su snack 1

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,9 +1,12 @@
+//Chiedo all'utente di inserire due numeri
 var firstNumber = parseInt(prompt('Inserisci un numero'));
 var secondNumber = parseInt(prompt('Inserisci un altro numero'));
 
+//Verifico che inserisca dei numeri e non altro
 if(isNaN(firstNumber) || isNaN(secondNumber)){
   console.log('Devi inserire un numero');
 }else{
+  
   if(firstNumber > secondNumber){
     console.log(firstNumber);
   }else{
