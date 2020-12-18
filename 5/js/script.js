339f051aff49c5092f29715bd53ec0c3b8ba24c1
FrensysThomas87 <delta873@gmail.com>
2020-12-18 18:00:13 +0100

    Inseriti commenti su snack 5

--- 5/js/script.js
+++ 5/js/script.js
@@ -1,8 +1,11 @@
 var arrVuoto = [];
 var numero;
 
+//Chiedo all'utente di inserire 6 numeri
 for(var i = 0; i < 6; i++){
   numero = parseInt(prompt('Inserisci un numero'));
+
+  //Se i numeri sono dispari vengono inseriti all'interno dell'array vuoto
   if(numero % 2 !== 0){
     arrVuoto.push(numero);
   }
