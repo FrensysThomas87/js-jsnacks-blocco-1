adeadc555bd03ec9d081d1a5e0cb9a96978b15e2
FrensysThomas87 <delta873@gmail.com>
2020-12-18 18:02:05 +0100

    Inseriti commenti su snack 6

--- 6/js/script.js
+++ 6/js/script.js
@@ -1,5 +1,6 @@
 var numero= parseInt(prompt('Inserisci un numero'));
 
+//Stampo il cubo dei numeri, esempio, se l'utente inserisce 5 stampero il cubo dei numeri che vanno da 1 a 5
 for(var i = 1; i <= numero; i++){
   console.log(Math.pow(i, 3));
 }
