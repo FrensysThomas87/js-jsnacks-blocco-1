f9f32d5916f6427e2a6772b51f596b5ac0eb528a
FrensysThomas87 <delta873@gmail.com>
2020-12-18 17:58:35 +0100

    Inseriti commenti su snack 4

--- 4/js/script.js
+++ 4/js/script.js
@@ -1,17 +1,21 @@
 var listaInvitati = ['Pippo', 'Pluto', 'Paperino', 'Topolino'];
+
+
 var nomeUtente = prompt('Come ti chiami?');
+
+//Faccio in modo che la prima lettera sia maiuscola e le altre minuscole
 nomeUtente = nomeUtente.charAt(0).toUpperCase() + nomeUtente.substring(1).toLowerCase();
 
 var invitato;
 
-
-
+//Controllo se il nome dell'utente è inserito all'interno dell'array
 for(var i = 0; i < listaInvitati.length && !invitato; i++){
   if(listaInvitati[i] === nomeUtente){
     invitato = true;
   }
 }
 
+// Se è presente può entrare altrimenti no
 if(invitato){
   console.log('Puoi entrare');
 }else{
